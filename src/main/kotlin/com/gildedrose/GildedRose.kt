8c9558dc83e5003a971e2526870c9b4feb62f050
Kelly Hong <kelly_hong@tutk.com>
2024-08-12 07:35:27 +0800

    origin one

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,69 +8,50 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (isRisingGoods(i)) {
-                if (couldSold(i) && isSellingGoods(i)) {
-                    decreaseQuality(i)
+            if (items[i].name != AGED_BRIE && items[i].name != CONCERT_BACKSTAGE_PASS) {
+                if (items[i].quality > 0 && items[i].name != SULFURAS) {
+                    items[i].quality -= 1
                 }
             } else {
                 if (items[i].quality < 50) {
-                    riseQuality(i)
+                    items[i].quality += 1
 
                     if (items[i].name == CONCERT_BACKSTAGE_PASS) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                riseQuality(i)
+                                items[i].quality += 1
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                riseQuality(i)
+                                items[i].quality += 1
                             }
                         }
                     }
                 }
             }
 
-            if (isSellingGoods(i)) {
+            if (items[i].name != SULFURAS) {
                 items[i].sellIn -= 1
             }
 
             if (items[i].sellIn < 0) {
-                if (isAgedBrie(i)) {
-                    if (isConcertBackstagePass(i)) {
-                        if (couldSold(i) && isSellingGoods(i)) {
-                            decreaseQuality(i)
+                if (items[i].name != AGED_BRIE) {
+                    if (items[i].name != CONCERT_BACKSTAGE_PASS) {
+                        if (items[i].quality > 0 && items[i].name != SULFURAS) {
+                            items[i].quality -= 1
                         }
                     } else {
                         items[i].quality -= items[i].quality
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        riseQuality(i)
+                        items[i].quality += 1
                     }
                 }
             }
         }
     }
-
-    private fun riseQuality(i: Int) {
-        items[i].quality += 1
-    }
-
-    private fun decreaseQuality(i: Int) {
-        items[i].quality -= 1
-    }
-
-    private fun couldSold(i: Int) = items[i].quality > 0
-
-    private fun isSellingGoods(i: Int) = items[i].name != SULFURAS
-
-    private fun isRisingGoods(i: Int) = isAgedBrie(i) && isConcertBackstagePass(i)
-
-    private fun isConcertBackstagePass(i: Int) = items[i].name != CONCERT_BACKSTAGE_PASS
-
-    private fun isAgedBrie(i: Int) = items[i].name != AGED_BRIE
-
 }
 
