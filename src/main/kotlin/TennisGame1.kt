20f4b20893059fcd65f736aed3114338a70576bf
Kelly Hong <kelly_hong@tutk.com>
2024-08-22 09:33:13 +0800

    Refactoring: tennis game 1.

--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -11,41 +11,30 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     }
 
     override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
-        } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
-            }
+        return when {
+            isDeuce() -> "Deuce"
+            isSamePoint() -> scoreMapToName(m_score1) + "-" + "All"
+            isSomeoneWinOrGoingToWin() -> checkWinner(m_score1 - m_score2)
+            else -> scoreMapToName(m_score1) + "-" + scoreMapToName(m_score2)
         }
-        return score
+    }
+
+    private fun isDeuce() = isSamePoint() && m_score1 >= 3
+    private fun isSamePoint() = m_score1 == m_score2
+    private fun isSomeoneWinOrGoingToWin() = m_score1 >= 4 || m_score2 >= 4
+
+    private fun checkWinner(minusResult: Int) = when {
+        minusResult == 1 -> "Advantage player1"
+        minusResult == -1 -> "Advantage player2"
+        minusResult >= 2 -> "Win for player1"
+        else -> "Win for player2"
+    }
+
+    private fun scoreMapToName(tempScore: Int) = when (tempScore) {
+        0 -> "Love"
+        1 -> "Fifteen"
+        2 -> "Thirty"
+        3 -> "Forty"
+        else -> ""
     }
 }
